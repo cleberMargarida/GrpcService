112cb1a86f42e3e1936f62065e21229d4f4fb174
Cleber Margarida <cleberfilho3@gmail.com>
2022-05-07 18:46:31 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7135");
+var client = new Client.ClientClient(channel);
+var reply = await client.GetAsync(
+                  new ClientRequest { Name = "1"});
+Console.WriteLine("Client: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+Console.WriteLine();
---
+++ GrpcService1/Services/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+
+namespace GrpcService1.Services
+{
+    public class ClientService : Client.ClientBase
+    {
+        private readonly ILogger<ClientService> _logger;
+     
+        public ClientService(ILogger<ClientService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ClientReply> Get(ClientRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ClientReply
+            {
+                Message = "Hello from Client"
+            });
+        }
+    }
+}
